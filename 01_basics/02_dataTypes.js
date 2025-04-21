82b96bb5a6b3d17c23392aba171cc74c8ce22242
Prachi Jethava <83333383+prachi1211@users.noreply.github.com>
2025-04-21 04:07:26 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version 
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+console.log("Prachi")
+
+let name = "prachi" //string
+let age = 18 //integer
+let isLoggedIn = false //boolean
+
+//number => 2 to power 53
+// big int is the number is too big
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => to find uniqueness
+
+// object
+console.log(typeof null)
+// the type of the null is object - interview question
+console.log(typeof undefined)
+// the type of the null is undefined - interview question
