4a971fd330b3fe854f721e56338522860f445a99
Prachi Jethava <83333383+prachi1211@users.noreply.github.com>
2025-04-22 02:20:03 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+// It checks both the datatype and the value as well. If both is equal then it will return True else False.
+
+console.log("2" === 2);
