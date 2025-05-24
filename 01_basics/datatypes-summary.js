08cbecf6f20ce2cbc960f3652a4c59484f5c262a
Prachi Jethava <83333383+prachi1211@users.noreply.github.com>
2025-05-24 22:06:19 +0000

    Stack and Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -24,9 +24,37 @@ console.log(id === anotherId);
 const heros= ['a', 'b', 'c']
 let myObj = {
     name: "prachi",
-    age= 23,
+    age:  23,
 }
-
+ 
 const myFunction = function(){
     console.log("Hello, Prachi");
 }
+
+// ================================================
+
+// Stack MEMORY (Primitive) you get the copy for all the changes
+//  Heap memory (Non- Primitive) here everything is changed in the orignal
+
+// In stack basically copy is given of rthe object so the orignal value is never changed.
+let myYouTubename = 'prachipiyushbhaijethava'
+let anotherName = 'khushijethava'
+
+console.log(myYouTubename)
+console.log(anotherName)
+
+// Here as you can see that the orignal value stays the same.
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Non-primitive
+// for this we know that it will give the reference for the orignal valuse thus when you change it then the orignal value is also changed.
+let userOne ={
+    email: "prachijethava@gmail.com",
+    age: 22,
+}
+
+let userTwo = userOne
+userTwo.email = "khushijethava@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+// Here you can see that both the emails for the users are changed.
