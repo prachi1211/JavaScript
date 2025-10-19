dd361332c9af838a570db7952b447a2c4b1d4011
prachi1211 <prachijethava1211@gmail.com>
2025-10-19 12:53:33 -0400

    update in dataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,28 @@
 let someNumber = 11
+
+// ways to convert number to string
+console.log(typeof someNumber)
+console.log(typeof (someNumber))
+
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1
+//false => 0 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; 
+// "prachi" => true
+// null => false
+// undefined => false
+// " " => true
+// NaN => false
 
 //*****************/ Operations **************************
 
